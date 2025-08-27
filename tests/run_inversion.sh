b562dccfef8b866f9d136e4ecdf0ac891bde42e7
Dmitry Antipov <dmitrij.antipov@gmail.com>
2025-08-27 17:07:50 -0400

    test fix

--- tests/run_inversion.sh
+++ tests/run_inversion.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 test=inversion
 rm tmp/*
-./TTT.py --gfa tests/$test/graph.noseq.gfa --coverage tests/$test/ont-coverage.csv --alignment tests/$test/alignments.gaf --outdir tmp --tangle-file tests/$test/tangle.ids   --num-initial-paths 3
+./TTT.py --graph tests/$test/graph.noseq.gfa --coverage tests/$test/ont-coverage.csv --alignment tests/$test/alignments.gaf --outdir tmp  --boundary-nodes tests/$test/border.ids  --num-initial-paths 10
 diff tmp/traversal.gaf tests/$test/expected_traversal.gaf
 python3 tools/diff_compare.py tmp/traversal.gaf tests/$test/expected_traversal.gaf
